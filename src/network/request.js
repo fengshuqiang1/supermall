c1543c3d41c293d68f58f649799d3683145ff73f
fengshuqiang <50195724+fengshuqiang1@users.noreply.github.com>
2020-05-05 21:11:46 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config){
     // 创建axios实例
     const instance = axios.create({
-      baseURL:'http://106.54.54.237:8000/api/h8',
+      baseURL:'',
       timeout:5000
     })
     // 进行数据请求
